eabcc399d575c4d6c95bfc83ad94e6461673d2ee
alllyuk <kornelyuk.lexa@yandex.ru>
2024-12-16 19:45:09 +0300

    add cross encoder reranker

--- src/reranker.py
+++ src/reranker.py
@@ -1,50 +1,50 @@
 # import numpy as np
 import hydra
-import re
+import torch
 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from omegaconf import DictConfig
-from typing import List
-from rank_bm25 import BM25Okapi
+from typing import List, Tuple
 from loguru import logger
+from retriever import retrieve_bm25 as rerank_bm25
 # from catboost import CatBoostRanker, Pool
 
 
-def rerank_bm25(cfg: DictConfig, query: str, chunks: List[str]):
-    pattern = r'''
-            [a-zA-Z_][a-zA-Z0-9_]*  # Идентификаторы (включая ключевые слова)
-            | \d+\.\d+              # Числа с плавающей точкой
-            | \d+                   # Целые числа
-            | "([^"\\]|\\.)*"       # Строковые литералы в двойных кавычках
-            | '([^'\\]|\\.)*'       # Строковые литералы в одинарных кавычках
-        '''
-    regex = re.compile(pattern)
-
-    tokenized_query = regex.findall(query)
-    corpus = [regex.findall(chunk) for chunk in chunks]
-
-    bm25 = BM25Okapi(corpus)
-    scores = bm25.get_scores(tokenized_query)
-
-    doc_scores = zip(chunks, scores)
-    doc_scores.sort(key=lambda doc_score: doc_score[1], reverse=True)
-    sorted_chunks = [chunk for chunk, _ in doc_scores]
-
-    return sorted_chunks
-
-
 def rerank_cross_encoder(cfg: DictConfig, query: str, chunks: List[str]):
-    pass
+    model_name = cfg['reranker']['model_name']
+    top_k = cfg['reranker']['top_k']
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(cfg['reranker']['model_name'])
+
+    inputs = tokenizer(
+        [[query, chunk] for chunk in chunks],
+        padding=True,
+        truncation=True,
+        return_tensors="pt"
+    )
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+        scores = outputs.logits[:, 0]  # Assuming binary classification: relevance score is the first logit
+
+    # Combine scores with corresponding chunks
+    chunk_scores = [(chunk, score.item()) for chunk, score in zip(chunks, scores)]
+    # Sort by scores in descending order
+    sorted_chunks = sorted(chunk_scores, key=lambda x: x[1], reverse=True)
+
+    return sorted_chunks[:top_k]
+
 
 
 def rerank_chunks(cfg: DictConfig, query: str, chunks: List[str]):
     try:
-        ranking_type = cfg['reranker']
-        if ranking_type == 'bm25':
+        reranker_type = cfg['reranker']
+        if reranker_type == 'bm25':
             sorted_chunks = rerank_bm25(cfg, query, chunks)
-        elif ranking_type == 'cross_encoder':
+        elif reranker_type == 'cross_encoder':
             sorted_chunks = rerank_cross_encoder(cfg, query, chunks)
         else:
-            raise ValueError(f'Unknown ranking type: {ranking_type}')
+            raise ValueError(f'Unknown ranking type: {reranker_type}')
 
         return sorted_chunks
 
