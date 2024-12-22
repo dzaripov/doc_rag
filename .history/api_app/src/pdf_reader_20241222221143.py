8b9d91f593837a40af2ab4b1474b08fad5c39c17
Ильяшенко Роман <r.ilyashenko@tbank.ru>
2024-12-22 23:55:19 +0300

    Деление на чанки

--- .history/api_app/src/pdf_reader_20241222221143.py
+++
@@ -1,4 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-def read_pdf(pdf_path):
-    
--- .history/api_app/src/pdf_reader_20241222221228.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-def read_pdf(pdf_path):
-    loader = PyPDFLoader(pdf_path)
-    pages = loader.load_and_split()
-    return pages
